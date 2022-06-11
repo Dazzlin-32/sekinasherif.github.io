3b05d6ff6ed9b38199ca8c5a088a88423e150bcd
Dazzlin-32 <74567138+Dazzlin-32@users.noreply.github.com>
2022-06-11 18:02:05 +0300

    Project page updated
    Project page updated added features and changed pictures

---
+++ Movie/trialphp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php and Html</title>
+    <link rel="stylesheet" href="css/bootstarp.min.css">
+    <script src="js/jquery-3.5.1.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+               <label>Student Number</label>
+               <input type="text " name="Student number">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
